ff94b17d675cf7ec8b4fd9a2a21776097ce2ef22
aTable <goforthejuggular@gmail.com>
2020-03-25 08:03:05 +1000

    testing

--- dist/main.js
+++ dist/main.js
@@ -5,14 +5,14 @@ var fetch2 = require('node-fetch');
 var core = require('@actions/core');
 var github = require('@actions/github');
 var glob = require('glob');
-var accessToken = core.getInput('DROPBOX_ACCESS_TOKEN') || process.env.DROPBOX_ACCESS_TOKEN;
+var accessToken = core.getInput('DROPBOX_ACCESS_TOKEN'); //|| process.env.DROPBOX_ACCESS_TOKEN
 if (!accessToken)
     core.setFailed('Error: missing DROPBOX_ACCESS_TOKEN');
 var dropbox = new Dropbox({ accessToken: accessToken, fetch: fetch2 });
-var globSource = core.getInput('GLOB') || 'sample/**/*.md';
+var globSource = core.getInput('GLOB'); //|| 'sample/**/*.md'
 if (!globSource)
     core.setFailed('Error: missing GLOB');
-var dropboxPathPrefix = core.getInput('DROPBOX_DESTINATION_PATH_PREFIX') || '/';
+var dropboxPathPrefix = core.getInput('DROPBOX_DESTINATION_PATH_PREFIX'); //|| '/'
 if (!dropboxPathPrefix)
     core.setFailed('Error: missing DROPBOX_DESTINATION_PATH_PREFIX');
 function uploadMuhFile(filePath) {
@@ -36,6 +36,6 @@ glob(globSource, {}, function (err, files) {
         console.log('all files uploaded', all);
     })
         .catch(function (err) {
-        console.error('something errored', err);
+        console.error('error', err);
     });
 });
